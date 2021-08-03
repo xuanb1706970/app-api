3d7233eb1365afff4d5efaae039a2ea89702e592
xuanb1706970 <xuanb1706970@student.ctu.edu.vn>
2021-08-03 11:34:32 +0700

    end

--- App.js
+++ App.js
@@ -1,93 +1,132 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Vibration } from 'react-native';
 import {Camera} from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
+import * as MediaLibrary from 'expo-media-library';
+import CameraButton from './component/CameraButton';
+import FlipButton from './component/CameraFlipBtn';
+import axios from 'axios';
 
-export default function App() {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [faces, setFaces] = useState([])
 
-    const faceDetector = ({faces}) =>{
-      if (faces.length >= 0){
-        setFaces(faces)
+
+export default class App extends React.Component{
+    constructor(){
+      super()
+      this.state = {
+        hasCameraPermissions: true,
+        type: Camera.Constants.Type.back,
+        showSnapBtn: false,
+        isLoading: false,
+        isVibrating: false,
+        faces: [],
+        predict: [],
+        isPreview: false,
+        path: []
+      }
+      this.camera = null
+      this.onFlip = this.onFlip.bind(this)
+      this.faceDetector = this.faceDetector.bind(this)
+      this.takeSnap = this.takeSnap.bind(this)
+    }
+
+    async ComponentWillMount(){
+      const {status} = await Camera.requestCameraPermissionsAsync()
+      this.setState = {
+        hasCameraPermissions:status === 'granted'
       }
     }
 
-    useEffect(()=>{
-      (async()=>{
-        const { status } = await Camera.requestCameraPermissionsAsync();
-        setHasPermission(status === 'granted');
-      })();
-    }, []);
+    onFlip() {
+      this.setState({
+        type: this.state.type === Camera.Constants.Type.back
+          ? Camera.Constants.Type.front
+          : Camera.Constants.Type.back
+      })
+    }
+
+    faceDetector = ({faces}) => {
+      if(faces.length > 0){
+        if(!this.state.isVibrating){
+          Vibration.vibrate(100)
+        }
+        this.setState({showSnapBtn: true, isVibrating: true})
+      }else{
+        this.setState({showSnapBtn: false, isVibrating: false})
+      }
+    }
+
+    async takeSnap(){
+      if(this.camera){
+        const options = { quality: 0.1, base64: true, mirrorImage: true };
+        let photo = await this.camera.takePictureAsync(options)
+        try{
+          var formdata = new FormData()
+          formdata.append('image',{
+            uri: photo.uri,
+            name: 'picture.jpg', 
+            type: 'image/jpg' 
+          })
+          const config = {
+            method: 'POST',
+            body: formdata
+          };
+  
+          const url = 'https://service-face.herokuapp.com/';
+          fetch(url,config)
+          .then((response) => response.text())
+          .then((responseEND)=>{
+             let name = responseEND.substring()
+             alert(name)
+          })
+          
+        }catch (error){
+          console.log('Failed to fetch products: ', error);
+        }
+        
+      }
+     }
+    
+  
+
+
+  render(){
+    const {showSnapBtn, type, hasCameraPermissions} = this.state;
+
+    if(!hasCameraPermissions){
+      return (<Text>Not Camerara</Text>)
+    }
 
     return(
-      <View style = {{flex: 1}}>
-        <Camera style={styles.camera}
-                type='front'
-                onFacesDetected={faceDetector}
-                faceDetectorSettings={{
-                  mode: FaceDetector.Constants.Mode.fast,
-                  detectLandmarks: FaceDetector.Constants.Landmarks.all,
-                  runClassifications: FaceDetector.Constants.Classifications.none,
-                }}>
-        </Camera>
-
-        <View style={styles.ViewButton}>     
-          <TouchableOpacity style={styles.button1}
-              onPress={() => this.snap(false)}>
-              <Text
-                  style={{ fontSize: 18, marginBottom: 5, color: 'black', textAlign: 'center'}}>
-                  {' '}Enroll{' '}
-              </Text>
-            </TouchableOpacity>
-            
-            <Text style={{ color: 'black' }}>{`Faces in view: ${faces.length}`}</Text>
-
-            <TouchableOpacity style={styles.button2}
-              onPress={() => this.snap(true)}>
-                  <Text
-                      style={{ fontSize: 18, marginBottom: 5, color: 'black', textAlign: 'center' }}>
-                      {' '}Recognize{' '}
-                  </Text>
-              </TouchableOpacity>
-          </View>  
-      </View>  
+        <View style={styles.wrapper}>
+            <Camera style={styles.wrapper}
+                    ref={ref=>{this.camera = ref}}
+                    type={type}
+                    onFacesDetected={this.faceDetector}
+                    faceDetectorSettings={{
+                      mode: FaceDetector.Constants.Mode.fast
+                    }}>
+             
+            <View style={styles.cameraContainer}>
+                <FlipButton onPress={this.onFlip} type={type} />
+                {showSnapBtn && <CameraButton iconName="camera" color="green" onPress={this.takeSnap} />}
+            </View>
+            </Camera>
+        </View>
     );
+  }
 
-}  
-
+}
 
 const styles = StyleSheet.create({
-  container: {
+
+   wrapper: {
+    flex: 1 
+  },
+  cameraContainer: {
     flex: 1,
     backgroundColor: 'transparent',
-    
-  },
-  camera: {
-    flex: 1,
-    justifyContent: 'space-between'
-    
-  },
-  ViewButton:{
-    width: '100%',
-    height: '10%',
-    alignItems: 'center',
     flexDirection: 'row',
-    backgroundColor: '#5D5E5C'
-  },
-  button1:{
-    width: '25%',
-    backgroundColor: 'white',
-    borderRadius: 5,
-    margin: 20
-    
-  },
-  button2:{
-    backgroundColor: 'white',
-    width: '25%',
-    borderRadius: 5,
-    margin: 20
+    justifyContent: "space-between"
   }
 });
-
